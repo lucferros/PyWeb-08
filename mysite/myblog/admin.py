e0e4f7cff786aeb95cd417ecd0cf47df40eef983
Michael Gregor <michaelgregor2600@gmail.com>
2016-05-22 19:10:49 -0700

    First attempt at setting up model admin with inlines

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-
 from myblog.models import Category
 from myblog.models import Post
 
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine,
+    ]
+admin.site.register(Category, CategoryAdmin)
 
-admin.site.register(Category)
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        PostInLine,
+    ]
+admin.site.register(Post, PostAdmin)
+
+
+class CategoryInLine(CategoryAdmin.TabularInline):
+    model = Category
+
+
+class PostInLine(PostAdmin.TabularInline):
+    model = Post
+
